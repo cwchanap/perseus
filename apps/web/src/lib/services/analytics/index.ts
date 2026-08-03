9fe2a5513b62f012380f62a0be18f3687f982d6d
Chan Wai Chan <cwchanap@connect.ust.hk>
2026-08-03 11:46:02 -0700

    docs: define analytics semantics and privacy gate

---
+++ apps/web/src/lib/services/analytics/index.ts
@@ -0,0 +1,36 @@
+export { createAnalyticsClient } from './analytics';
+export type { AnalyticsClient, AnalyticsClientErrorCode } from './analytics';
+
+export {
+	buildAnalyticsClientContextV1,
+	buildAnalyticsPuzzleContextV1,
+	classifyAspectBucket,
+	classifyAssistanceMode,
+	classifyPieceCountBucket,
+	classifyPrimaryInput,
+	classifyProgressBucket,
+	classifyViewportClass,
+	resolveAuthenticationClass,
+	resolveContentOrigin
+} from './context';
+export type {
+	AnalyticsAuthStatus,
+	AssistanceUsageSnapshot,
+	PrimaryInputSnapshot
+} from './context';
+
+export { createAnalyticsRunLedger } from './run-ledger';
+export type {
+	AnalyticsLedgerMarkResult,
+	AnalyticsRunLedger,
+	AnalyticsRunLedgerErrorCode,
+	AnalyticsRunLedgerMarkInputV1
+} from './run-ledger';
+
+export type { AnalyticsTransport } from './transport';
+export { createHttpAnalyticsTransport } from './transports/http';
+export {
+	createMemoryAnalyticsTransport,
+	type MemoryAnalyticsTransport
+} from './transports/memory';
+export { createNoopAnalyticsTransport } from './transports/noop';
